782c842bb3fc72a62ceebf29a46b112fe5824729
Paco Gimeno <pgimeno@made-f0Ar5s11.clusters.42paris.fr>
2024-06-10 13:40:13 +0200

    File_test folder renamed and main fixed

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: pgimeno <pgimeno@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 14:53:24 by pgimeno           #+#    #+#             */
-/*   Updated: 2024/06/10 09:40:56 by pgimeno          ###   ########.fr       */
+/*   Updated: 2024/06/10 13:38:49 by pgimeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,36 +27,8 @@ int main(int argc, char **argv)
 		count++;
 		printf("%s", line);
 		free(line);
-		break ;
-	}
-	close(file);
-
-	count = 0;
-
-	file = open(argv[1], O_RDWR);
-	while ((line = get_next_line(file)))
-	{
-		count++;
-		printf("%s", line);
-		free(line);
 	}
 	printf("\nLine Count: %d", count);
-/*	get_next_line(file);
-	get_next_line(file);
-	get_next_line(file);
-	get_next_line(file);
-	get_next_line(file);
-	get_next_line(file);*/
-/*	printf("%s", get_next_line(file));
-	printf("%s", get_next_line(file));
-	printf("%s", get_next_line(file));
-	printf("%s", get_next_line(file));
-	printf("%s", get_next_line(file));
-	printf("%s", get_next_line(file));
-	printf("%s", get_next_line(file));
-	printf("%s", get_next_line(file));
-	printf("%s", get_next_line(file));*/
 	close(file);
-
-  return (0);
+	return (0);
 }
